d86e5ba41655569f546566101d5bbfe92f5c36e4
Levskar <exelnabul@abv.bg>
2023-03-23 23:10:34 +0200

    start

---
+++ Lego_and_other_toys_Project/Lego_and_other_toys_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lego_and_other_toys_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
