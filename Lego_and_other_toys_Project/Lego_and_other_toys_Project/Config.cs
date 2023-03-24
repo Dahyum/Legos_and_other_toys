f10cd63d9b80fca8e662f1dd94058af11f08cf35
Dahyum <accdjani3@gmail.com>
2023-03-24 19:20:07 +0200

    connection and context added

---
+++ Lego_and_other_toys_Project/Lego_and_other_toys_Project/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lego_and_other_toys_Project
+{
+    static class Config
+    { 
+        public static string connectionString = "Server=LAPTOP-65NCVK5L\\SQLEXPRESS;Database=VideoGameReviewLibrary;Integrated Security=True";
+    }
+}
---
+++ Lego_and_other_toys_Project/Lego_and_other_toys_Project/ToyStore.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lego_and_other_toys_Project
+{
+    public class ToyStore : DbContext
+    {
+        public ToyStore()
+        {
+
+        }
+
+        public ToyStore(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<LegoSet> LEGOSets { get; set; }
+
+        public virtual DbSet<SportToy> SportsToys { get; set; }
+
+        public virtual DbSet<BoardGame> BoardGames { get; set; } 
+
+        public virtual DbSet<BabyToy> BabyToys { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer(Config.connectionString);
+            
+            base.OnConfiguring(dbContextOptionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            base.OnModelCreating(modelbuilder);
+        }
+
+    }
+}
